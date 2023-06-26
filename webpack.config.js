99c7712bc7aadc72ef51feb004ac2431e06c1bf3
Thomas Bakken <tbakken14@gmail.com>
2023-06-26 11:32:24 -0700

    Add template files

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    devtool: false,
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+        new ESLintPlugin(),
+        new CleanWebpackPlugin({
+            verbose: true
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Template Project',
+            template: './src/index.html',
+            inject: 'body'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
